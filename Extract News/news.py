ec883f180517a13f40950202370b3010762ba3b7
rahulrameshm0 <iamrahulm23@gmail.com>
2025-06-16 20:47:01 +0530

    code updated and set to autamate automatically every day at 9:00 AM

--- Extract News/news.py
+++ Extract News/news.py
@@ -3,6 +3,13 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import pandas as pd
 import time
+from datetime import datetime
+import os
+import sys
+
+application_path = os.path.dirname(sys.executable)
+now = datetime.now()
+moth_day_year = now.strftime('%m%d%y')
 
 
 website = "https://www.thesun.co.uk/sport/football/"
@@ -37,9 +44,12 @@ for container in containers:
     except:
         continue
 my_dict = {'title': titles, 'subtitle': subtitles, 'link': links}
-
 headlines = pd.DataFrame(my_dict)
-headlines.to_csv('headless.csv', index=False)
+
+file_name = f'headline{moth_day_year}.csv'
+final_path = os.path.join(application_path, file_name)
+
+headlines.to_csv(final_path)
 drive.quit()
 
 
