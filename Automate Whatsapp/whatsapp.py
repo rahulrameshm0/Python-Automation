bf77b5782378b578fbff215a372e6046621e1666
rahulrameshm0 <iamrahulm23@gmail.com>
2025-07-10 12:17:08 +0530

    automation for whatsapp.py

--- Automate Whatsapp/whatsapp.py
+++ Automate Whatsapp/whatsapp.py
@@ -1,18 +1,5 @@
-# import pywhatkit
-#
-# phone_number = input("Enter your phone number: ")
-# pywhatkit.sendwhatmsg(phone_number, "Hey! How you doing", 15, 13,15, True, 2)
+import pywhatkit
 
-a = [1,2,3,4,5,6,7,8,9,10]
-user = int(input("Enter a number: "))
-b = 5
-for i in a:
-    if i == user:
-        print(a.index(user))
-        break
-else:
-    c = 0
-    for j in enumerate(a):
-        if j != a:
-            print(len(j))
-            break
+phone_number = input("Enter your phone number: ")
+pywhatkit.sendwhatmsg(phone_number, "Hey! How you doing", 15, 13, wait_time=15, tab_close=True)
+
