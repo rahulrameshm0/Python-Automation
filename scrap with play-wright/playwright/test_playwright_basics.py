d1744845b19a8cb48581ed679a5ba61e84740104
rahulrameshm0 <iamrahulm23@gmail.com>
2025-08-03 00:12:13 +0530

    learnig basics of playwright

--- scrap with play-wright/playwright/test_playwright_basics.py
+++ scrap with play-wright/playwright/test_playwright_basics.py
@@ -1,6 +1,22 @@
+import time
 
+from playwright.sync_api import Page, expect
 def test_playwright(playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context() #To do some operation, login ->
     page = context.new_page()
     page.goto('https://rahulshettyacademy.com')
+
+def test_playwrightShortcut(page:Page):
+    page.goto('https://rahulshettyacademy.com')
+
+def test_coreLocators(page:Page):
+    page.goto('https://rahulshettyacademy.com/loginpagePractise/')
+    page.get_by_label('Username:').fill('rahulshettyacademy')
+    page.get_by_label('Password:').fill('learning')
+    page.get_by_role('combobox').select_option('teach')
+    page.get_by_role('link',name='terms and conditions').click()
+    page.locator('#terms').check()
+    page.get_by_role('button', name='Sign In').click()
+    expect(page.get_by_text('Incorrect username/password.')).to_be_visible()
+    time.sleep(6)
