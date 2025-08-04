c27273af4fd4325038407e40455673023a6daabe
rahulrameshm0 <iamrahulm23@gmail.com>
2025-08-04 16:06:18 +0530

    learned automating card items

---
+++ scrap with play-wright/playwright/test_UIValidation_1.py
@@ -0,0 +1,23 @@
+from playwright.sync_api import Page, Playwright, expect
+import time
+
+def test_UIValidationDynamicScript(page:Page):
+
+    page.goto('https://rahulshettyacademy.com/loginpagePractise/')
+    page.get_by_label('Username:').fill('rahulshettyacademy')
+    page.get_by_label('Password:').fill('learning')
+    page.get_by_role('combobox').select_option('teach')
+    page.locator('#terms').check()
+    page.get_by_role('button', name='Sign In').click()
+
+    # Iphone X
+    iphone = page.locator('app-card').filter(has_text='iphone X')
+    iphone.get_by_role('button').click()
+
+    #Nokia Edge
+    nokia_edge = page.locator('app-card').filter(has_text='Nokia Edge')
+    nokia_edge.get_by_role('button').click()
+
+    page.get_by_text('Checkout').click()
+    expect(page.locator('.media-body')).to_have_count(2)
+    time.sleep(6)
