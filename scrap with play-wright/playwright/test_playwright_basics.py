e5472ce2c532fa43a2275198805e54f5b3d2d444
rahulrameshm0 <iamrahulm23@gmail.com>
2025-08-04 14:52:13 +0530

    automating in firefox

--- scrap with play-wright/playwright/test_playwright_basics.py
+++ scrap with play-wright/playwright/test_playwright_basics.py
@@ -1,6 +1,6 @@
 import time
 
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page, expect, Playwright
 def test_playwright(playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context() #To do some operation, login ->
@@ -16,6 +16,20 @@ def test_coreLocators(page:Page):
     page.get_by_label('Password:').fill('learning')
     page.get_by_role('combobox').select_option('teach')
     page.get_by_role('link',name='terms and conditions').click()
+    page.locator('#terms').check()
+    page.get_by_role('button', name='Sign In').click()
+    expect(page.get_by_text('Incorrect username/password.')).to_be_visible()
+    time.sleep(6)
+
+def test_fireFoxbrowser(playwright:Playwright):
+    fire_fox = playwright.firefox
+    browser = fire_fox.launch(headless=False)
+    page = browser.new_page()
+    page.goto('https://rahulshettyacademy.com/loginpagePractise/')
+    page.get_by_label('Username:').fill('rahulshettyacademysdas')
+    page.get_by_label('Password:').fill('learning')
+    page.get_by_role('combobox').select_option('teach')
+    page.get_by_role('link', name='terms and conditions').click()
     page.locator('#terms').check()
     page.get_by_role('button', name='Sign In').click()
     expect(page.get_by_text('Incorrect username/password.')).to_be_visible()
