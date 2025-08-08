15f3e6580b66a977d7d39455bb697b81051fc6af
rahulrameshm0 <iamrahulm23@gmail.com>
2025-08-08 16:05:15 +0530

    learning how to extract data from the table

--- scrap with play-wright/playwright/test_MoreValidation.py
+++ scrap with play-wright/playwright/test_MoreValidation.py
@@ -13,6 +13,24 @@ def test_UIChecks(page:Page):
     page.on('dialog', lambda dialog: dialog.accept())
     page.get_by_role('button', name='Confirm').click()
 
-    time.sleep(5)
-
     # Frame handling
+    page_frame = page.frame_locator('#courses-iframe')
+    page_frame.get_by_role('link', name='All Access plan').click()
+    expect(page_frame.locator('body')).to_contain_text('All Access Subscription')
+
+    # Check the price of rise is the equal to 37
+    # Identify the price column
+    # Extract the price of the rice
+
+    page.goto('https://rahulshettyacademy.com/seleniumPractise/#/offers')
+
+    for index in range(page.locator('th').count()):
+        if page.locator('th').nth(index).filter(has_text='Price').count()>0:
+            price_value = index
+            print(f'The price of the rise is {price_value}')
+            break
+
+    rice_row = page.locator('tr').filter(has_text='Rice')
+    if rice_row == page.locator('td'):
+        print(rice_row)
+    time.sleep(5)
